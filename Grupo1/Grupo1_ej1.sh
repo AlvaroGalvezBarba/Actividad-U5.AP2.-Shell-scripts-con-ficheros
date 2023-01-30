8b25e13c7e666f7152a8e15f7072f8dd99a3bdf8
ALVARO GALVEZ <algaba01@floridauniversitaria.es>
2023-01-30 09:59:26 +0100

    Emepzamos Grupo1_ej1 y acabamos Grupo1_ej2

--- Grupo1/Grupo1_ej1.sh
+++ Grupo1/Grupo1_ej1.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "Introduce el numero de subcarpetas: "$1
-
-echo $1
+subcarpetas=$1
+rm -r nombres
+mkdir nombres
+cd nombres
+cd ..
+for (( i=0; i>=10; ++i ))
+do
+    while read line
+    do 
+        $nombre==$line;
+    done < nombres.txt
+done
+cd nombres
+for (( i=0; i>=$subcarpetas; ++i ))
+do
+    mkdir $nombre${i}
+    mkdir personal${i}
+    cd ..
+done
---
+++ Grupo1/Grupo1_ej2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while read numero
+do 
+    if [ $(($numero % 2)) -eq 0 ]
+        then
+            echo "El numero $numero es par"
+            let par=$par+1;
+            else
+            echo "El numero $numero es impar"
+            let impar=$impar+1;
+    fi
+done < numeros.txt
+echo "$par, $impar"
