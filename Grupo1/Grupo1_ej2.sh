d6ac19d7cd165086c4f4dac5c0c9a62849155bc4
ALVARO GALVEZ <algaba01@floridauniversitaria.es>
2023-02-02 08:36:58 +0100

    Empezamos el Grupo1_ej3

--- Grupo1/Grupo1_ej2.sh
+++ Grupo1/Grupo1_ej2.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 while read numero
-do 
-    if [ $(($numero % 2)) -eq 0 ]
-        then
-            echo "El numero $numero es par"
-            let par=$par+1;
-            else
-            echo "El numero $numero es impar"
-            let impar=$impar+1;
+do
+    if [ "$(($numero % 2)) -eq 0 "];then
+        echo "El numero $numero es par"
+        let par=$par+1;
+        else
+        echo "El numero $numero es impar"
+        let impar=$impar+1;
     fi
 done < numeros.txt
 echo "$par, $impar"
